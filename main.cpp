d8a75386d67a0cd93c322d58c60c626447351615
ChetashMeher <31277513+ChetashMeher@users.noreply.github.com>
2017-08-30 16:21:20 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int num1,num2,sub;
+
+    cout << "enter your number1 = " << endl;
+    cin >> num1;
+    cout << "enter your number 2 =" << endl;
+    cin >> num2 ;
+    sub = num1-num2 ;
+    cout << " sub is " << sub << endl ;
+
+    return 0;
+}
